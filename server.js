e4577728d140d9d97640d07def295bcb840d082d
Jehyun <congmul@hotmail.com>
2020-12-07 14:54:17 -0800

    Add server.js & mysql DB

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+
+const app = express();
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(() =>{
+    app.listen(PORT, () =>{
+      console.log("App listening on PORT " + PORT);
+    });
+  });
+  
