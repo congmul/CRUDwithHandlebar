c713b0ed53f78c99ae2913ea106b1d9dc997fd64
Jehyun <congmul@hotmail.com>
2020-12-07 15:03:03 -0800

    Add port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
 const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
