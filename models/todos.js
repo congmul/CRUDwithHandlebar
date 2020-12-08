76f9edc81ccc42f8b8207ed7b36793c1358577ca
Jehyun <congmul@hotmail.com>
2020-12-07 19:05:44 -0800

    Add validation

--- models/todos.js
+++ models/todos.js
@@ -1,6 +1,21 @@
+// module.exports = function(sequelize, DataTypes) {
+//     const Todo = sequelize.define("todo", {
+//         text: DataTypes.STRING, 
+//         complete: DataTypes.BOOLEAN
+//     });
+
+//     return Todo;
+// }
+
 module.exports = function(sequelize, DataTypes) {
     const Todo = sequelize.define("todo", {
-        text: DataTypes.STRING, 
+        text: {
+            type: DataTypes.STRING,
+            validate: {
+                // notNull: true
+                len: [1, 140]
+            }
+        },
         complete: DataTypes.BOOLEAN
     });
 
